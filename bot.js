d4b16dd9357f352e4f30274ec3bb78aac3b8e957
joshphxeon <113538993+joshphxeon@users.noreply.github.com>
2022-09-17 22:47:42 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
+global.owner = ['923177537350',''] //owner number +27634090203
+global.wame = ['923177537350'] 
+global.pemilik = ['923177537350'] //owner number +27634090203
+global.premium = ['923177537350'] //premium number +27634090203
+global.pengguna = 'Muzamil' //username
+global.footer = 'Xeon BOT INC'
+global.botnma = 'Xeon BOT INC' //bot name
 global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.ownernma = 'Muzamil' //owner name
+global.packname = 'Muzamil xeon' //sticker package name
+global.author = 'By Xeon Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
